805571e50b759d994786efc71d2e483f27e6614c
Paul Nykiel <paul.nykiel@gmail.com>
2022-07-17 20:51:02 +0200

    Added association strategy as parameter to manager

--- src/EkfSlamManager.hpp
+++ src/EkfSlamManager.hpp
@@ -33,13 +33,16 @@ namespace ekf_slam {
         }
 
         auto update(typename Vehicle::Meas vehicleMeas, const std::vector<typename Object::Meas> &objectMeasurements,
-                    double dt) -> std::pair<typename Vehicle::State, std::vector<typename Object::State>> {
+                    double dt,
+                    typename EKF::AssociationFunc associationFunc =
+                            association::nearest_neighbor_association<Object::Meas::DIM, T>)
+                -> std::pair<typename Vehicle::State, std::vector<typename Object::State>> {
             this->dt = dt;
             auto z_vehicle = vehicleMeas.getVec();
             std::vector<typename Object::Meas::Vec> z_object;
             std::transform(objectMeasurements.cbegin(), objectMeasurements.cend(), std::back_inserter(z_object),
                            [](const typename Object::Meas &meas) { return meas.getVec(); });
-            ekf.update(z_vehicle, z_object, association::nearest_neighbor_association<Object::Meas::DIM, T>);
+            ekf.update(z_vehicle, z_object, associationFunc);
 
             typename Vehicle::State vehicleState{ekf.getVehicle()};
             std::vector<typename Object::State> objects;
